92935eb1641f3f52834b41e0c8c6c8f6308bbdeb
Alexandrbig1 <alexsmagin1@gmail.com>
2025-04-11 12:52:51 -0500

    feat: add services: for user, email, auth and upload file with cloudinary

--- src/conf/config.py
+++ src/conf/config.py
@@ -7,6 +7,26 @@ class Settings(BaseSettings):
     POSTGRES_PASSWORD: str
     POSTGRES_HOST: str
     POSTGRES_PORT: int
+    
+    # jwt
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+    ALGORITHM: str = "HS256"
+    SECRET_KEY: str = "secret"
+    # redis
+    REDIS_URL: str = "redis://localhost"
+    # mail
+    MAIL_USERNAME: EmailStr = ""
+    MAIL_PASSWORD: str = ""
+    MAIL_FROM: EmailStr = ""
+    MAIL_PORT: int = 465
+    MAIL_SERVER: str = "smtp.meta.ua"
+    MAIL_FROM_NAME: str = "Rest API Service"
+    MAIL_STARTTLS: bool = False
+    MAIL_SSL_TLS: bool = True
+    USE_CREDENTIALS: bool = True
+    VALIDATE_CERTS: bool = True
+
 
     @property
     def DB_URL(self) -> str:
